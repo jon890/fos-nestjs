66adabf6c6c11207fc2ed095638cda51c48f2604
jon890 <jon89071@gmail.com>
2025-11-24 16:52:35 +0900

    ➕ Opentelemetry 통합을 위한 라이브러리 추가

--- src/middleware/request-logging.middleware.ts
+++ src/middleware/request-logging.middleware.ts
@@ -1,12 +1,17 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { trace } from '@opentelemetry/api';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class RequestLoggingMiddleware implements NestMiddleware {
-  private logger: Logger = new Logger(RequestLoggingMiddleware.name);
+  private logger: Logger = new Logger(RequestLoggingMiddleware.name, {
+    timestamp: true,
+  });
+
+  private readonly tracer = trace.getTracer('fos-nestjs', '1.0.0');
 
   use(req: Request, res: Response, next: NextFunction) {
-    this.logger.log(`Request - Method: ${req.method}, URL: ${req.baseUrl}`);
+    this.logger.log(`[Request] Method : ${req.method}, URL: ${req.baseUrl}`);
     next();
   }
 }
